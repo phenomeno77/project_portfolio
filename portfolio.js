b5195a13e4fab82a3ad3bb120d9d837ffc6c279e
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2022-09-06 10:42:31 +0200

    changes made on portfolio layout

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
     });
 
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
 });
