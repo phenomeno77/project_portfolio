be807d1c714d7be62d4c8e89c18165e1c945b7f2
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2022-09-08 01:14:12 +0200

    faded overlay added

--- portfolio.js
+++ portfolio.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
         theme: "minimal"
     });
 
-    $('#dismiss, .overlay').on('click', function () {
+    $('#dismiss, .overlay').click(function () {
         $('#sidebar').removeClass('active');
         $('.overlay').removeClass('active');
+        $('#sidebar').hide('slow');
     });
 
-    $('#sidebarCollapse').on('click', function () {
+    $('#sidebarCollapse').click(function () {
         $('#sidebar').addClass('active');
         $('.overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        $('#sidebar').show('slow');
     });
 });
